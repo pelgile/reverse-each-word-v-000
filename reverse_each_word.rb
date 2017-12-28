ec361502901b427ec28213d0aef9e6a6aefa483f
Michelle Wiraydi <miwiryadi@gmail.com>
2017-12-28 14:42:21 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,3 @@ def reverse_each_word(sentence)
   sentence.split.collect {|word| word.reverse}.join(" ")
 end
 
-# Without iterating:
-
-# def reverse_each_word(sentence)
-#  sentence.reverse.split.reverse.join(" ")
-# end
